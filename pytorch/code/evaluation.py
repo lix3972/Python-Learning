160a38c717ac3a47357e60804651c0d63dec94df
lix3972 <35994518+lix3972@users.noreply.github.com>
2020-04-17 00:07:35 +0800

    Update evaluation.py

--- pytorch/code/evaluation.py
+++ pytorch/code/evaluation.py
@@ -1,4 +1,5 @@
 import torch as th
+import torchvision.transforms as transforms
 
 
 # mask的batchsize必须是1, 或者就是3个维度 2个维度 统一变成3个维度。
@@ -29,19 +30,12 @@ def unnorm(x):  # 输入必须是三个维度(c, w, h)
         std_list.append(0.5)
     mean = th.tensor(mean_list)
     std = th.tensor(std_list)
-    # mean = th.tensor([0.5])
-    # std = th.tensor([0.5])
 
     return (std * x.permute(1, 2, 0) + mean).permute(2, 0, 1)
 
 
-# 遇到mask标准化的问题
-def seg_eval(mask_pre, mask_gt, background=False):
-    """
-    :param mask_pre: type(mask_pre)=torch.tensor, shape=(1, c, w, h) or (c, w, h), 预测mask
-    :param mask_gt: mask真值,type,shape同上
-    :return: 语义分割评价标准PA：正确分类点数/像素点总数
-    """
+# 遇到mask标准化的问题 如果mask就是一个背景一个前景,gt_max=1.如果多个前景gt_max=不包括背景的分类数
+def seg_eval(mask_pre, mask_gt, gt_max=1):
     mask_pre, c_pre, w_pre, h_pre = shape_cal(mask_pre)
     mask_gt, c_gt, w_gt, h_gt = shape_cal(mask_gt)
     mask_pre = unnorm(mask_pre)
@@ -51,6 +45,8 @@ def seg_eval(mask_pre, mask_gt, background=False):
     # 但预测mask_pre是小数 当其为1个通道时需要取阀值使其非零即一,多通道时某位置的在多个通道上的最大值对应的层编号为该位置的值
     if c_gt > 1:  # 真值 分成多个通道
         mask_gt = th.argmax(mask_gt, 0)
+    else:
+        mask_gt = mask_gt * gt_max  # mask_gt多个这都归一化到0～1,乘以gt_max在扩到多个值.
     if c_pre > 1:  # 预测值
         mask_pre = th.argmax(mask_pre, 0)  # mask0: 1 chanel, value: 0, 1, 2, ...
     else:  # 预测mask是一个通道 但是值不是整数的情况
@@ -59,53 +55,27 @@ def seg_eval(mask_pre, mask_gt, background=False):
     mask_pre = mask_pre.to(dtype=th.int)
     # 将统一好的mask在变回多通道的情况 此时mask_pre也均为整数了
     obj_ids = th.unique(mask_gt)  # obj_ids对于 mask_pre也是一样
-    # obj_ids = obj_ids.to(dtype=th.int)
 
     masks_gt = mask_gt == obj_ids[:, None, None]  # dtype = torch.uint8, shape = (class, w, h),class包括背景0
     masks_pre = mask_pre == obj_ids[:, None, None]  # dtype = torch.uint8
 
-    # 计算PA
-    # pii = mask_pre == mask_gt
-    # PA = pii.sum().item() / (w_pre*h_pre)  # PA(Pixel Accuracy)
-
-    # PA(Pixel Accuracy), MPA(Mean PA), IOU(Intersection over Union)
-    pii_pij = 0
-    intersection = 0
-    union = 0
     pii_pij_list = []
     intersection_list = []
-    union_list =[]
-    # ##################################################################
-    # 既然obj_ids是mask_gt含有的值 怎么会有一项为0 该项为0与含有该项的值是矛盾的
-    # 出现分母为0的原因是如果i是torch.uint8类型，
-    # 在使用类似mask[i]方法时会出现其shape=(0, 1, 512, 512)并且mask[i].sum()=0的奇怪情况
-    # ##################################################################
-    for i in obj_ids:
+    union_list = []
+
+    for i in range(obj_ids.shape[0]):
+        ### print('masks_gt.shape={}, masks_pre.shape={}, obj_ids={}, i={}'.format(masks_gt.shape, masks_pre.shape, obj_ids, i))
 
         # pytorch没有找到逻辑与运算 用两步代替
         logic0 = masks_pre[i] + masks_gt[i]  # 都为1的地方相加=2, 1和0的位置=1, 0和0的位置=0
         p_and = logic0 > 1  # >1即=2的 为逻辑与的结果,也是正确分类的部分pii
         # 求或
         p_or = logic0 > 0  # >0 为逻辑或的结果
-        # pij_sum = masks_gt[i].sum().item() 注：不加item()会导致计算结果异常
-        # 需要处理 分母为0 的情况
-        # 计算pii_pij分母为零 masks_gt[i].sum().item()=0意味着该图片不含该分类 求平均时应该算少一个分类
-        # 计算Iou分母为零 union=0意味着预测值与真值同时为零 是预测准确的情况
 
         pii_pij_list.append(p_and.sum().item() / masks_gt[i].sum().item())
-        # pii_pij += p_and.sum().item() / masks_gt[i].sum().item()
-
         intersection_list.append(p_and.sum().item())
-        # intersection += p_and.sum().item()
-
-        # union += masks_pre[i] + masks_gt[i] - pii
         union_list.append(p_or.sum().item())
-        # union += p_or.sum().item()
 
-    # PA = intersection / (w_pre*h_pre)
-    # MPA = pii_pij / obj_ids.shape[0]
-    # IoU = intersection / union
-    # 包含背景和前景
     PA = sum(intersection_list) / (w_pre*h_pre)
     MPA = sum(pii_pij_list) / len(pii_pij_list)
     IoU = sum(intersection_list) / sum(union_list)
@@ -129,6 +99,13 @@ def seg_eval(mask_pre, mask_gt, background=False):
 if __name__ == '__main__':
     # s相当于mask_gt; (s0,s1,s2)是mask_gt分为3层的结果; p是mask_pre(按最大值)合为1层的效果
     # (p0,p1,p2)是mask_pre的三层;(y0,y1,y2)是mask_pre合为1层(取最大值)再分成3层的结果;
+    # 类别  分类正确个数  gt个数  pre并gt个数
+    # 0     2           5       7
+    # 1     6           6       7
+    # 2     3           5       7
+    # PA = (2+6+3)/16=11/16=0.6875 ;         PAf = (6+3)/(6+5)=9/11=0.81818 ; PAb = 2/5=0.4
+    # MPA = (2/5 + 6/6 + 3/5)/3=2/3=0.6667 ; MPAf = (6/6 + 3/5)/2=1.6/2=0.8 ; MPAb = 2/5=0.4
+    # IoU = (2+6+3)/(7+7+7)=11/21=0.5238 ;   IoUf = (6+3)/(7+7)=9/14=0.6428 ; IoUb = 2/7=0.2857
     s = [[0, 0, 1, 0], [1, 1, 1, 0], [1, 1, 2, 2], [0, 2, 2, 2]]
     p = [[0, 0, 1, 1], [1, 1, 1, 2], [1, 1, 2, 0], [2, 2, 2, 0]]
 
@@ -144,8 +121,12 @@ if __name__ == '__main__':
     s2 = [[0,     0,   0,   0], [0,     0,   0,   0], [0,     0,   1,   1], [0,     1,   1,   1]]
     y2 = [[0,     0,   0,   0], [0,     0,   0,   1], [0,     0,   1,   0], [1,     1,   1,   0]]
 
-    gt = th.tensor(s)
-    pre = th.tensor([p0, p1, p2])
-    PAf, MPAf, IoUf, PAb, MPAb, IoUb, PA, MPA, IoU = seg_eval(pre, gt)
-    print('PA={}, MPA={}, IoU={}'.format(PA, MPA, IoU))
+    gt = th.tensor(s).unsqueeze(0).to(dtype=th.float)
+    pre = th.tensor([p0, p1, p2]).to(dtype=th.float)
+    gt_norm = transforms.Normalize((0.5,), (0.5,))(gt/gt.max())
+    pre_norm = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(pre)
+    PAf, MPAf, IoUf, PAb, MPAb, IoUb, PA, MPA, IoU = seg_eval(pre_norm, gt_norm, gt.max())
+    print('all: PA={}, MPA={}, IoU={}'.format(PA, MPA, IoU))
+    print('fore: PAf={}, MPAf={}, IoUf={}'.format(PAf, MPAf, IoUf))
+    print('back: PAb={}, MPAb={}, IoUb={}'.format(PAb, MPAb, IoUb))
     print('ok')
