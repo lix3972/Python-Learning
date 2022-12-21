672e78feb04ce90fe88fd3d16d38e528f93a80f7
lix3972 <35994518+lix3972@users.noreply.github.com>
2022-12-21 19:38:34 +0800

    Update onehot.py

--- pytorch/code/FCN_easiest_master/onehot.py
+++ pytorch/code/FCN_easiest_master/onehot.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def onehot(data, n):
+def onehot_bak(data, n):
     buf = np.zeros(data.shape + (n, ))
     nmsk = np.arange(data.size)*n + data.ravel()  # mask 中的1 放入0,2,4...(第0层)
     buf.ravel()[nmsk-1] = 1  # mask中的0, 作为1,放入另外一层
